f0c9d39b958d4a6ad27179fb1f9e18ba0dd015c9
Rui Santos <cardkingrui@gmail.com>
2020-07-12 14:19:21 -0400

    Scene Changer script

---
+++ PR_PoisonPong_2D/Assets/_Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void SceneChange(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
